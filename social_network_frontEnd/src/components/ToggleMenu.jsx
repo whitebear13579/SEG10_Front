6ea7db00a3327feb076a60a3f945538354e3879c
plate <37104323+ray9487@users.noreply.github.com>
2024-11-23 14:17:56 +0800

    friendsearch
    layout

--- social_network_frontEnd/src/components/ToggleMenu.jsx
+++ social_network_frontEnd/src/components/ToggleMenu.jsx
@@ -26,7 +26,8 @@ function ToggleMenu() {
         <a href="#home">聊天室</a>
         <a href="#userInfo">設定</a>
         <a href="#friendList">好友列表</a>
-        <a href="#friendRequest">好友請求及搜尋</a>
+        <a href="#friendRequest">好友請求</a>
+        <a href="#friendSearch">好友搜尋</a>
         <a href="#login" onClick={handleLogout}>登出</a>
       </div>
     </div>
--- social_network_frontEnd/src/page/FriendRequestPage.jsx
+++ social_network_frontEnd/src/page/FriendRequestPage.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import ToggleMenu from '../components/ToggleMenu';
-import SearchFriend from "../components/SearchFriend";
 export default function FriendRequestPage(){
     return (
         <>
-        <SearchFriend />
         </>
     )
 };

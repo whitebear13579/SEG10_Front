3d963689c262cf678e9f5bacbb3477243b998356
Brian Tseng <bbt2587898123@gmail.com>
2024-12-08 18:07:34 +0800

    處理朋友請求後會移除

--- social_network_frontEnd/src/components/FriendRequest.jsx
+++ social_network_frontEnd/src/components/FriendRequest.jsx
@@ -1,83 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../assets/page/friendRequest.css";
 import ToggleMenu from "./ToggleMenu";
+
 function FriendRequest() {
+  const [fndRsps, setFndRsps] = useState([
+    { name: "企鵝四號", id: 0 },
+    { name: "企鵝五號", id: 1 },
+    { name: "企鵝六號", id: 2 },
+    { name: "企鵝七號", id: 3 },
+    { name: "企鵝八號", id: 4 },
+  ]);
+
+  const handleResponse = (id, isAccept) => {
+    if (isAccept) {
+      console.log(`Friend request accepted for id: ${id}`);
+      // Add logic to add the friend (if needed)
+    } else {
+      console.log(`Friend request ignored for id: ${id}`);
+      // Logic for ignoring the friend request
+    }
+    // Remove the request from the list
+    const newFndRsps = fndRsps.filter((fndRsp) => fndRsp.id !== id);
+    setFndRsps(newFndRsps);
+  };
+
   return (
     <>
       <ToggleMenu />
       <div className="FriendRequest-container">
         <h1 className="title">好友請求</h1>
-        <hr></hr>
-        <div className="friendrequest">
-          <img src="images/penguin-png.png" />
-          <div className="friendName">企鵝四號</div>
-          <button className="agree"></button>
-          <button className="disagree"></button>
+        <hr />
+        <div className="friend-requests">
+          {fndRsps.map((fndRsp) => (
+            <div key={fndRsp.id} className="friendrequest">
+              <img src="images/penguin-png.png" alt="Penguin" />
+              <div className="friendName">{fndRsp.name}</div>
+              <button
+                className="agree"
+                onClick={() => handleResponse(fndRsp.id, true)}
+              >
+              </button>
+              <button
+                className="disagree"
+                onClick={() => handleResponse(fndRsp.id, false)}
+              >
+              </button>
+            </div>
+          ))}
         </div>
-        <hr></hr>
-        <div className="friendrequest">
-          <img src="images/penguin-png.png" />
-          <div className="friendName">企鵝五號</div>
-          <button className="agree"></button>
-          <button className="disagree"></button>
-        </div>
-        <hr></hr>
-        <div className="friendrequest">
-          <img src="images/penguin-png.png" />
-          <div className="friendName">企鵝六號</div>
-          <button className="agree"></button>
-          <button className="disagree"></button>
-        </div>
-        <hr></hr>
-        <div className="friendrequest">
-          <img src="images/penguin-png.png" />
-          <div className="friendName">企鵝六號</div>
-          <button className="agree"></button>
-          <button className="disagree"></button>
-        </div>
-        <hr></hr>
-        <div className="friendrequest">
-          <img src="images/penguin-png.png" />
-          <div className="friendName">企鵝六號</div>
-          <button className="agree"></button>
-          <button className="disagree"></button>
-        </div>
-        <hr></hr>
-        <div className="friendrequest">
-          <img src="images/penguin-png.png" />
-          <div className="friendName">企鵝六號</div>
-          <button className="agree"></button>
-          <button className="disagree"></button>
-        </div>
-        <hr></hr>
-        <div className="friendrequest">
-          <img src="images/penguin-png.png" />
-          <div className="friendName">企鵝六號</div>
-          <button className="agree"></button>
-          <button className="disagree"></button>
-        </div>
-        <hr></hr>
-        <div className="friendrequest">
-          <img src="images/penguin-png.png" />
-          <div className="friendName">企鵝六號</div>
-          <button className="agree"></button>
-          <button className="disagree"></button>
-        </div>
-        <hr></hr>
-        <div className="friendrequest">
-          <img src="images/penguin-png.png" />
-          <div className="friendName">企鵝六號</div>
-          <button className="agree"></button>
-          <button className="disagree"></button>
-        </div>
-        <hr></hr>
-        <div className="friendrequest">
-          <img src="images/penguin-png.png" />
-          <div className="friendName">企鵝六號</div>
-          <button className="agree"></button>
-          <button className="disagree"></button>
-        </div>
-        <hr></hr>
       </div>
     </>
   );
---
+++ social_network_frontEnd/src/components/ResponseFndRequest.jsx
@@ -0,0 +1,10 @@
+import "../assets/page/friendRequest.css";
+export default function ResponseFndRequest(){
+
+    return (
+        <>
+            <button className="agree"></button>
+            <button className="disagree"></button>
+        </>
+    );
+};
